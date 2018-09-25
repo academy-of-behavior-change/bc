1c483bffa381badd0305d8504199e0adb9eaff89
matherion <gjalt-jorn@behaviorchange.eu>
2018-09-25 14:43:56 +0200

    Added manual page for the package

---
+++ R/behaviorchange.R
@@ -0,0 +1,15 @@
+#' @details
+#' Contains specialised analyses and
+#' visualisation tools for behavior change science.
+#' These facilitate conducting determinant studies
+#' (for example, using confidence interval-based
+#' estimation of relevance, CIBER, or CIBERlite
+#' plots) and systematically developing, reporting,
+#' and analysing interventions (for example, using
+#' acyclic behavior change diagrams). This package
+#' is especially useful for researchers in the field of
+#' behavior change or health psychology and to
+#' behavior change professionals such as intervention
+#' developers and prevention workers.
+#' @keywords internal
+"_PACKAGE"
